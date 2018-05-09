583cbe3ff903ac9fa48b9a74812150b9db313c71
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-09 10:50:48 +0200

    Adding a New item to the UI PART I : Create HTML string with placeholder text

--- app.js
+++ app.js
@@ -92,6 +92,24 @@ UIController =  (function() {
             };
         },
 
+        addListItem: function(obj, type) {
+
+            var html;
+            
+            // Create HTML string with placeholder text
+            if (type === 'inc') {
+                html = '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                html = '<div class="item clearfix" id="expense-0"><div class="item__description">Apartment rent</div><div class="right clearfix"><div class="item__value">- 900.00</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+
+            // Replace the placeholder text with some actual data
+
+            // Insert the HTML into DOM
+
+        },
+
         // Exposing the DOM strings object into the public
 
         getDOMstrings: function() {
