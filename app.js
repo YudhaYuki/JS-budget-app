79165ad7ba6329f6277980f52755b5cafb5aa717
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-09 16:49:33 +0200

    Setting up the DELETE EVENT LISTENER using EVENT DELEGATION PART III - Structuring the plans

--- app.js
+++ app.js
@@ -224,6 +224,7 @@ var controller = (function(budgetCtrl, UICtrl) {
             } 
         });
 
+        // Event delegation
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     };
 
@@ -275,6 +276,14 @@ var controller = (function(budgetCtrl, UICtrl) {
             splitID = itemID.split('-');
             type = splitID[0];
             ID = splitID[1];
+
+            // 1. Delete the item from the data structure
+
+
+            // 2. Delete the item from the UI
+
+
+            // 3. Update and show the new budget 
 
         }
 
