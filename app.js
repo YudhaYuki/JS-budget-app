ec4fa6beff70455ede8962f37e8642b7dbb8465d
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-09 11:15:43 +0200

    Adding a New item to the UI PART III - Adjection: Insert the HTML into DOM

--- app.js
+++ app.js
@@ -79,7 +79,10 @@ UIController =  (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
+        
     };
 
     return {
@@ -94,12 +97,15 @@ UIController =  (function() {
 
         addListItem: function(obj, type) {
 
-            var html, newHTML;
+            var html, newHTML, element;
             
             // Create HTML string with placeholder text
             if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+
                 html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
 
@@ -109,7 +115,7 @@ UIController =  (function() {
             newHTML = newHTML.replace('%value%', obj.value);       
 
             // Insert the HTML into DOM
-
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
         // Exposing the DOM strings object into the public
