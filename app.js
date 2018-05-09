062c16a82e8808b9fa3fae74e5e7930d57e7453c
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-09 10:15:01 +0200

    Adding a New Item to our Budget Controller PART IV - If conditional for ID, and now test in console by typing budgetController.testing()

--- app.js
+++ app.js
@@ -43,7 +43,11 @@ var budgetController = (function() {
             var newItem, ID;
 
             // Create new ID
-            ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;                
+            } else {
+                ID = 0;
+            }
 
             // Create new item based on 'inc' or 'exp' type
             if (type === 'exp') {
@@ -57,7 +61,11 @@ var budgetController = (function() {
 
             // Return the new element
             return newItem;
+        },
 
+        // New method to access PRIVATE DATA structure (Just testing)
+        testing: function() {
+            console.log(data);
         }
     };
 
